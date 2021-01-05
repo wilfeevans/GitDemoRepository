e73e2c751a0ef8ceb9802ae79085eefe399d3c79
wilfred <wilfeevans@gmail.com>
2021-01-05 23:45:36 +0530

    my first commit

---
+++ src/GitPackage/GitTest1.java
@@ -0,0 +1,19 @@
+package GitPackage;
+
+import java.util.Scanner;
+
+public class GitTest1 {
+
+	public static void main(String[] args) 
+	{
+	
+		int a=20;
+		int b=30;
+		int sum=a + b;
+		
+		System.out.println("Adding two numbers:" + sum);
+		
+
+	}
+
+}
