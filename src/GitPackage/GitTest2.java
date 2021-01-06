7c12e3aa984ab5d718e7ac913bb0c3200a397cc4
wilfred <wilfeevans@gmail.com>
2021-01-06 15:32:39 +0530

    Added GitTest2

---
+++ src/GitPackage/GitTest2.java
@@ -0,0 +1,19 @@
+package GitPackage;
+
+import java.util.Scanner;
+
+public class GitTest2 {
+
+	public static void main(String[] args) 
+	{
+	
+		int a=120;
+		int b=130;
+		int sum=a + b;
+		
+		System.out.println("Adding two numbers:" + sum);
+		
+
+	}
+
+}
